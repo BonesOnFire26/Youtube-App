93ca73d292fe80c08f779255dbb5d3b64628a8cd
Aidan Schmidt <aidanschmidt@Aidans-MacBook-Pro.local>
2020-06-14 15:20:04 -0700

    initial xcode project creation

---
+++ Youtube-App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Youtube-App
+//
+//  Created by Aidan Schmidt on 6/12/20.
+//  Copyright © 2020 Aidan Schmidt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
